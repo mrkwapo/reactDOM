7c9f1780cf4a72db227b481cd36819735dd27f33
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-12 18:28:55 -0400

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// import React from 'react'; //you would use this with line 3 but since we are using line 4 we use the code on line 2
+import React, { Component } from 'react';
+
+// function App() { //this is the same as line 4 and 5
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1> My first React App!</h1>
+        <p>Welcome ;)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
