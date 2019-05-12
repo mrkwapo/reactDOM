d506dfeeff4da2c036288c8d1cdbcf4b77d6bd93
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-12 19:07:21 -0400

    Add files via upload

---
+++ my-app/src/Ancestry.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ancestry extends Component {
+  render() {
+    return (
+      <div className='ancestor'>
+        <div>Name: Buddy</div>
+        <div>Age: 30</div>
+        <div>Birth Year: 1984</div>
+      </div>
+    );
+  }
+}
+
+export default Ancestry;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 // import React from 'react'; //you would use this with line 3 but since we are using line 4 we use the code on line 2
 import React, { Component } from 'react';
+import Ancestry from './Ancestry';
 
 // function App() { //this is the same as line 4 and 5
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className='App'>
         <h1> My first React App!</h1>
         <p>Welcome ;)</p>
+        <Ancestry />
       </div>
     );
   }
