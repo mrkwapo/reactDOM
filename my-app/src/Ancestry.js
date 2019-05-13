4bbb0aba7f6a2bfe2b228fb72398ffdab4365630
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-13 00:20:34 -0400

    added conditionals

--- my-app/src/Ancestry.js
+++ my-app/src/Ancestry.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
 const Ancestry = ({ ancestors }) => {
-  const ancestorList = ancestors.map(ancestor => {
-    return (
-      <div className='ancestor' key={ancestor.id}>
-        <div>Name: {ancestor.name}</div>
-        <div>Age: {ancestor.age}</div>
-        <div>Birthday: {ancestor.birthday}</div>
-      </div>
-    );
-  });
-  return <div className='ancestor-list'>{ancestorList}</div>;
+  //   const ancestorList = ancestors.map(ancestor => {
+  //     if (ancestor.age > 30) {
+  //       return (
+  //         <div className='ancestor' key={ancestor.id}>
+  //           <div>Name: {ancestor.name}</div>
+  //           <div>Age: {ancestor.age}</div>
+  //           <div>Birthday: {ancestor.birthday}</div>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+
+  return (
+    <div className='ancestor-list'>
+      {ancestors.map(ancestor => {
+        return ancestor.age > 30 ? (
+          <div className='ancestor' key={ancestor.id}>
+            <div>Name: {ancestor.name}</div>
+            <div>Age: {ancestor.age}</div>
+            <div>Birthday: {ancestor.birthday}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Ancestry;
