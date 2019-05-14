26bb0b7bf15a53b65f0d8447409c6352801eb5f9
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-13 20:02:23 -0400

    added CSS

--- my-app/src/Ancestry.js
+++ my-app/src/Ancestry.js
@@ -1,33 +1,43 @@
 import React from 'react';
+import './ancestors.css';
 
-const Ancestry = ({ ancestors }) => {
-  //   const ancestorList = ancestors.map(ancestor => {
-  //     if (ancestor.age > 30) {
-  //       return (
-  //         <div className='ancestor' key={ancestor.id}>
-  //           <div>Name: {ancestor.name}</div>
-  //           <div>Age: {ancestor.age}</div>
-  //           <div>Birthday: {ancestor.birthday}</div>
-  //         </div>
-  //       );
-  //     } else {
-  //       return null;
-  //     }
-  //   });
+const Ancestry = ({ ancestors, deleteAncestor }) => {
+  const ancestorList = ancestors.map(ancestor => {
+    if (ancestor.age > 30) {
+      return (
+        <div className='ancestor' key={ancestor.id}>
+          <div>Name: {ancestor.name}</div>
+          <div>Age: {ancestor.age}</div>
+          <div>Birthday: {ancestor.birthday}</div>
+          <button
+            onClick={() => {
+              deleteAncestor(ancestor.id);
+            }}
+          >
+            Delete ancestor
+          </button>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
 
-  return (
-    <div className='ancestor-list'>
-      {ancestors.map(ancestor => {
-        return ancestor.age > 30 ? (
-          <div className='ancestor' key={ancestor.id}>
-            <div>Name: {ancestor.name}</div>
-            <div>Age: {ancestor.age}</div>
-            <div>Birthday: {ancestor.birthday}</div>
-          </div>
-        ) : null;
-      })}
-    </div>
-  );
+  return <div className='ancestor-list'>{ancestorList}</div>;
 };
 
+// return (
+//   <div className='ancestor-list'>
+//     {ancestors.map(ancestor => {
+//       return ancestor.age > 30 ? (
+//         <div className='ancestor' key={ancestor.id}>
+//           <div>Name: {ancestor.name}</div>
+//           <div>Age: {ancestor.age}</div>
+//           <div>Birthday: {ancestor.birthday}</div>
+//         </div>
+//       ) : null;
+//     })}
+//   </div>
+// );
+// };
 export default Ancestry;
